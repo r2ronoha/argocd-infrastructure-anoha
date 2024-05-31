520cb22b07131a4790590629eb86dbb09385f95d
Katarzyna Rupiewicz <katarzyna.rupiewicz@jamf.com>
2024-05-31 15:55:14 +0200

    JSC-59441 add ssm-read module

--- infrastructure/modules/ssm-read/variables.tf
+++ infrastructure/modules/ssm-read/variables.tf
@@ -1,9 +1,11 @@
 variable "ssm_vpc_path" {
     description = "path to vpc_id in AWS SSM Parameter Store"
+    sensitive = true
     type = string
 }
 
 variable "ssm_sn_path" {
     description = "path to database subnets id in AWS SSM Parameter Store"
+    sensitive = true
     type = string
 }
